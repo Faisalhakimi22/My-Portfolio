2e42bbde8ec5ed432c742d6697e256c710205829
Faisalhakimi22 <faisalh5556@gmail.com>
2024-08-21 00:56:22 +0500

    Update SkillText component with improved styling and responsiveness

--- components/main/hero.tsx
+++ components/main/hero.tsx
@@ -1,15 +1,13 @@
-"use client";
-
 import { HeroContent } from "@/components/sub/hero-content";
 
 export const Hero = () => {
   return (
-    <div className="relative flex flex-col h-screen w-full overflow-hidden">
+    <div className="relative flex flex-col h-full w-full">
       <video
         autoPlay
         muted
         loop
-        className="absolute inset-0 w-full h-full object-cover -z-10"
+        className="absolute inset-100x top-[-400px] left-0 w-full h-full object-cover -z-20"
       >
         <source src="/videos/p.mp4" type="video/mp4" />
       </video>
