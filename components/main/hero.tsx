be8ba666c1efe03ce1036d4beb99a6c9baaecc2f
Faisalhakimi22 <faisalh5556@gmail.com>
2024-10-05 22:40:57 +0500

    Initial commit: Replace video with image in Hero component

--- components/main/hero.tsx
+++ components/main/hero.tsx
@@ -3,16 +3,18 @@ import { HeroContent } from "@/components/sub/hero-content";
 
 export const Hero = () => {
   return (
-    <div className="relative flex flex-col h-full w-full">
-      <Image
-        src="/videos/p.jpg" // You need to provide an absolute path for the image
-        alt="Hero Background"
-        layout="fill" // This makes the image take up the entire space of its container
-        objectFit="cover" // Ensures the image covers the container without distortion
-        priority // Loads the image with high priority, good for above-the-fold images
-        className="-z-20"
-      />
-      
+    <div className="relative flex flex-col h-screen w-full">
+      <div className="relative w-full h-full">
+        <Image
+          src="/videos/p.jpg"
+          alt="Hero Background"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center" // Ensures the image is centered within the container
+          priority
+          className="-z-20"
+        />
+      </div>
       <HeroContent />
     </div>
   );
