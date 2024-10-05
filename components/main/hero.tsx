a30ebfcc5f729f83786bae74bcda37af1622ac8c
Faisalhakimi22 <faisalh5556@gmail.com>
2024-10-05 23:44:50 +0500

    Initial commit: Replace video with image in Hero component

--- components/main/hero.tsx
+++ components/main/hero.tsx
@@ -6,7 +6,7 @@ export const Hero = () => {
       <img
         src="videos/p.jpg"  // Use forward slashes for paths
         alt="Hero Background"
-        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-auto h-auto max-w-full max-h-[80vh] -z-20" // Maintain aspect ratio while limiting height
+        className="absolute inset-0 object-contain w-full h-full -z-20 mt-[-10%]" // Adjust margin to move image up
       />
       
       <HeroContent />
