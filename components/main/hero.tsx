77c2bedb6c82150be37f897798efaca14d70bd6a
Faisalhakimi22 <faisalh5556@gmail.com>
2024-10-06 00:43:54 +0500

    Initial commit: Replace video with image in Hero component

--- components/main/hero.tsx
+++ components/main/hero.tsx
@@ -2,7 +2,7 @@ import { HeroContent } from "@/components/sub/hero-content";
 
 export const Hero = () => {
   return (
-    <div className="relative flex flex-col h-full w-full overflow-hidden">
+    <div className="relative flex flex-col h-full w-full">
       <img
         src="videos/p.jpg" // Use forward slashes for paths
         alt="Hero Background"
